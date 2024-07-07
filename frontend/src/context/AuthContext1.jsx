e9a2c8851970e58277ef38301d5e663020ba341e
Lamiaaaaaaaaaa <ferdausjannatul301@gmail.com>
2024-07-07 19:33:54 +0600

    logout function, incomplete profile setup and changing pfp

--- frontend/src/context/AuthContext1.jsx
+++ frontend/src/context/AuthContext1.jsx
@@ -1,4 +1,4 @@
-import { createContext, useReducer} from 'react'
+import { createContext, useReducer } from 'react'
 
 export const AuthContext = createContext()
 
@@ -14,7 +14,7 @@ export const authReducer = (state, action) => {
 }
 
 export const AuthContextProvider = ({ children }) => {
-    const [state, dispatch] = userReducer(authReducer, {
+    const [state, dispatch] = useReducer(authReducer, {
         user:null
     })
     console.log('AuthContext state: ', state)
